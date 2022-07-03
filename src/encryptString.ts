552a0dc305d5f95a836fa4ade586887fdef1ddae
amitshky <a7shky@gmail.com>
2022-07-03 12:36:38 +0545

    update

--- src/encryptString.ts
+++ src/encryptString.ts
@@ -12,14 +12,14 @@ const encrypt = async (plaintext: string, password: string): Promise<string> =>
 	const pbkdf2Key: CryptoKey = await crypto.subtle.importKey('raw', passwordU8Arr, { name: 'PBKDF2' }, false, ['deriveBits']);
 	const pbkdf2KeyU8Arr: Uint8Array = new Uint8Array(await crypto.subtle.deriveBits({ name: 'PBKDF2', iterations: 10000, salt: pbkdf2Salt, hash: 'SHA-256' }, pbkdf2Key, 384));
 
-	const key: CryptoKey = await crypto.subtle.importKey('raw', pbkdf2KeyU8Arr.slice(0, 32), { name: 'AES-CBC', length: 256 }, true, ['encrypt']);
+	const key: CryptoKey = await crypto.subtle.importKey('raw', pbkdf2KeyU8Arr.slice(0, 32), { name: 'AES-CBC', length: 256 }, false, ['encrypt']);
 	const iv: Uint8Array = pbkdf2KeyU8Arr.slice(32);
 
 	const encryptedU8Arr: Uint8Array = new Uint8Array(await crypto.subtle.encrypt({ name: 'AES-CBC', iv: iv }, key, plaintextU8Arr));
 	const cipherU8Arr = new Uint8Array(encryptedU8Arr.length + 16);
 	cipherU8Arr.set(enc.encode('Salted__'));
-	cipherU8Arr.set(pbkdf2Salt, 8)
-	cipherU8Arr.set(encryptedU8Arr, 16)
+	cipherU8Arr.set(pbkdf2Salt, 8);
+	cipherU8Arr.set(encryptedU8Arr, 16);
 	const ciphertext: string = Buffer.from(cipherU8Arr).toString('base64');
 
 	return ciphertext;
