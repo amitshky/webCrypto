34281aa94de9fc7a51d52a19a9899bbfca55e907
amitshky <a7shky@gmail.com>
2022-07-04 12:50:16 +0545

    browserify bundle

--- src/rsaEncryption.ts
+++ src/rsaEncryption.ts
@@ -1,4 +1,4 @@
-import { webcrypto } from 'node:crypto';
+import { webcrypto } from 'crypto';
 const crypto = webcrypto as unknown as Crypto;
 
 (async () =>
@@ -13,13 +13,13 @@ const crypto = webcrypto as unknown as Crypto;
 		hash: { name: 'SHA-256' }
 	}, true, ['encrypt', 'decrypt']);
 
-	const rsaPrivateKey = Buffer.from(await crypto.subtle.exportKey('pkcs8', rsaKey.privateKey!)).toString('base64');
-	const rsaPublicKey = Buffer.from(await crypto.subtle.exportKey('spki', rsaKey.publicKey!)).toString('base64');
+	const rsaPrivateKey: string = Buffer.from(await crypto.subtle.exportKey('pkcs8', rsaKey.privateKey!)).toString('base64');
+	const rsaPublicKey: string = Buffer.from(await crypto.subtle.exportKey('spki', rsaKey.publicKey!)).toString('base64');
 	console.log('Private Key:', rsaPrivateKey, '\n');
 	console.log('Public Key:', rsaPublicKey, '\n');
 
-	const cipher = await crypto.subtle.encrypt({ name: 'RSA-OAEP' }, rsaKey.publicKey!, plaintext);
-	const ciphertext = Buffer.from(cipher);
+	const cipher: any = await crypto.subtle.encrypt({ name: 'RSA-OAEP' }, rsaKey.publicKey!, plaintext);
+	const ciphertext: Buffer = Buffer.from(cipher);
 	console.log('ciphertext:', ciphertext.toString('base64'), '\n');
 
 	const plaintextBytes = await crypto.subtle.decrypt({ name: 'RSA-OAEP' }, rsaKey.privateKey!, ciphertext);
--- src/rsaSignature.ts
+++ src/rsaSignature.ts
@@ -4,6 +4,8 @@ const crypto = webcrypto as unknown as Crypto;
 (async () =>
 {
 	let plaintext: Buffer = Buffer.from('hello', 'utf-8');
+	console.log('plaintext:', plaintext.toString('utf-8'), '\n');
+	
 	const rsaKey: CryptoKeyPair = await crypto.subtle.generateKey({ 
 		name: 'RSASSA-PKCS1-v1_5',
 		modulusLength: 2048, // The length in bits of the RSA modulus. This should be at least 2048
@@ -11,11 +13,14 @@ const crypto = webcrypto as unknown as Crypto;
 		hash: { name: 'SHA-256' }
 	}, true, ['sign', 'verify']);
 
-	//const rsaPrivateKey = await crypto.subtle.exportKey('pkcs8', rsaKey.privateKey!);
-	//const rsaPublicKey  = await crypto.subtle.exportKey('spki', rsaKey.publicKey!);
+	const rsaPrivateKey: string = Buffer.from(await crypto.subtle.exportKey('pkcs8', rsaKey.privateKey!)).toString('base64');
+	const rsaPublicKey: string  = Buffer.from(await crypto.subtle.exportKey('spki', rsaKey.publicKey!)).toString('base64');
+	console.log('Private key:', rsaPrivateKey, '\n');
+	console.log('Public key:', rsaPublicKey, '\n');
 
 	// sign plaintext
-	const signature = await crypto.subtle.sign({ name: 'RSASSA-PKCS1-v1_5' }, rsaKey.privateKey!, plaintext);
+	const signature: ArrayBuffer = await crypto.subtle.sign({ name: 'RSASSA-PKCS1-v1_5' }, rsaKey.privateKey!, plaintext);
+	console.log('Signature:', Buffer.from(signature).toString('base64'), '\n');
 
 	// verify plaintext
 	const isVerified: boolean = await crypto.subtle.verify({ name: 'RSASSA-PKCS1-v1_5' }, rsaKey.publicKey!, signature, plaintext);
